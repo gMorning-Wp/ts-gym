9773f5838330a8eaa65a11762dda2d597a292863
wangpeng180 <wangpeng180@jd.com>
2024-02-23 10:10:39 +0800

    feat: 添加parameter的实现

--- challenges/03312-easy-parameters/template.ts
+++ challenges/03312-easy-parameters/template.ts
@@ -2,3 +2,4 @@ type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer
 
 
 //  infer的使用， 对于参数进行 假设 X 占位 如果条件满足则返回 占位X
+// https://github.com/Microsoft/TypeScript/pull/24897
